484baaa5b7012408280c5ba025030211f737d7a2
Benjamín Serrano <benser22@hotmail.com>
2025-12-15 11:22:44 -0300

    update server configuration for host

--- backend/config/server.ts
+++ backend/config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("STRAPI_URL"),
+  url: env("STRAPI_URL", "https://rano-api.22studios.xyz"), // Fallback to explicitly secure URL
   proxy: true,
   app: {
     keys: env.array("APP_KEYS"),
